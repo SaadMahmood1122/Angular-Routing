d172abe8a606565ebedd796d0ba68c9aecd44f8d
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-23 22:21:36 +0500

    Setting Up & Navigating To Dynamic Routes

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  //the userId property must be same as u define in url
+  userId = input.required<string>();
+
+  private userServie = inject(UsersService);
+  userName = computed(() => {
+    return this.userServie.users.find((user) => user.id === this.userId())
+      ?.name;
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { TasksComponent } from './app/tasks/tasks.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, withComponentInputBinding())],
 }).catch((err) => console.error(err));
