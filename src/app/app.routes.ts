baf5c15d80efbd6e4a53694c9454a062ccf4fd0a
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-24 00:24:43 +0500

    Working with Nested Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
@@ -10,5 +11,16 @@ export const routes = [
   {
     path: 'users/:userId', //<your-domain>/users/<uid>
     component: UserTasksComponent,
+    children: [
+      {
+        // will append the parent route
+        path: 'tasks', //<your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new', //<your-domain>/users/<uid>/tasks/new
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
