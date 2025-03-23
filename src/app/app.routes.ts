1ff66ff9d42008a6ba169f0771d2565a1764215e
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-24 02:18:54 +0500

    Adding Not Found Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -22,5 +23,9 @@ export const routes = [
         component: NewTaskComponent,
       },
     ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
