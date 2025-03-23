1e89e9d611985f53911b7656e05e55db3736e679
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-24 01:15:52 +0500

    Accessing Parent Route Data From Inside Nested Routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { TasksComponent } from './app/tasks/tasks.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(
+      routes,
+      // allow to get value dynamic value from url and assign to input declare in component
+      withComponentInputBinding(),
+      // this ftn will able the chile component to get input value from parent url
+      withRouterConfig({ paramsInheritanceStrategy: 'always' })
+    ),
+  ],
 }).catch((err) => console.error(err));
